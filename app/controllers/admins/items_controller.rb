973c763c11f0f34c83cf4c54b05824fb728cd6e0
ryohei19 <ryohei.basket@gmail.com>
2021-08-17 10:01:27 +0000

    商品登録機能　アクション作成

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,2 +1,18 @@
 class Admins::ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
+  end
+
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :sale_status)
+  end
 end
