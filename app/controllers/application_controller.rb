3fdc530b546dd954c62a20fff8811aceb6898880
memorialize1 <tenkiyohousha@gmail.com>
2021-08-19 07:49:52 +0000

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  def after_sign_in_path_for(resource)
-    case resource
-    when Admin
-      admins_items_path          #pathは設定したい遷移先へのpathを指定してください
-    when Customer
-      root_path              #ここもpathはご自由に変更してください
-    end
-  end
-
 end
