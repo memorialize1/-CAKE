83508070bf81823185f457a8ebe48210ae3e0e84
ryohei19 <ryohei.basket@gmail.com>
2021-08-24 06:06:31 +0000

    [admin]会員一覧・詳細　作成

--- app/controllers/admins/customer_dates_controller.rb
+++ app/controllers/admins/customer_dates_controller.rb
@@ -1,2 +1,19 @@
 class Admins::CustomerDatesController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:l_name, :f_name, :l_kana, :f_kana, :post_code, :address, :phone_number, :email, :user_status)
+  end
+
 end
