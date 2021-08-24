e53f6d34c8381c65e6029dd39f76d6b47cebb58f
ryohei19 <ryohei.basket@gmail.com>
2021-08-24 07:29:21 +0000

    [admin]会員情報編集　　作成

--- app/controllers/admins/customer_dates_controller.rb
+++ app/controllers/admins/customer_dates_controller.rb
@@ -11,6 +11,15 @@ class Admins::CustomerDatesController < ApplicationController
     @customer = Customer.find(params[:id])
   end
 
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admins_customer_dates_path
+    else
+      render :edit
+    end
+  end
+
   private
   def customer_params
     params.require(:customer).permit(:l_name, :f_name, :l_kana, :f_kana, :post_code, :address, :phone_number, :email, :user_status)
