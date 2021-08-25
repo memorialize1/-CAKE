d57ac0c1b304bb7f55075259479e075c540769de
EC2 Default User <ec2-user@ip-172-31-61-195.ec2.internal>
2021-08-25 08:00:10 +0000

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_074935) do
+ActiveRecord::Schema.define(version: 2021_08_17_053930) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -100,11 +100,6 @@ ActiveRecord::Schema.define(version: 2021_08_19_074935) do
     t.integer "shipping", default: 800, null: false
     t.integer "total_price", null: false
     t.integer "order_status", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
