172921c127732f17ed404dc23f1b444c62d9fc0f
ryohei19 <ryohei.basket@gmail.com>
2021-08-26 18:05:55 +0000

    [admin]注文履歴一覧・詳細　作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,19 +1,19 @@
 class Order < ApplicationRecord
-  
+
   belongs_to :customer
-  has_many   :order_details, dependent: :destroy
-  
+  has_many   :order_dates, dependent: :destroy
+
   enum order_status: [:入金待ち, :入金確認, :製作中, :発送準備中, :発送済み]
   enum payment: [:クレジットカード, :銀行振込]
   #ステータスの番号振り。左から0.1.2...と割り振られる。
-  
+
    after_update do
     if self.order_status == "入金確認"
-      self.order_details.each {|order_detail|
-      order_detail.update(item_status: "製作待ち")
+      self.order_dates.each {|order_date|
+      order_date.update(item_status: "製作待ち")
       }
     end
   end
-  
-  
+
+
 end
